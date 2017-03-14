5ae1d9ea057f3762485e44e24a369b04fd5d18ce
Valentin <valentin.delys@gmail.com>
2017-03-14 21:51:22 +0100

    Added ClarkWright class

--- src/dii/vrp/tp/ClarkWright.java
+++ src/dii/vrp/tp/ClarkWright.java
@@ -3,12 +3,23 @@ package dii.vrp.tp;
 import dii.vrp.data.IDemands;
 import dii.vrp.data.IDistanceMatrix;
 
-public class ClarkWright {
-    public ClarkWright(IDistanceMatrix matrix, IDemands demands, double Q) {
+/**
+ * Created by Valentin on 12/03/2017.
+ */
+public class ClarkWright
+{
 
+    private IDistanceMatrix distances;
+    private IDemands demands;
+    private double Q;
+
+    public ClarkWright(IDistanceMatrix distances, IDemands demands, double Q){
+        this.distances = distances;
+        this.demands = demands;
+        this.Q = Q;
     }
 
-    public ISolution run() {
+    public ISolution run(){
         return new VRPSolution();
     }
 }
