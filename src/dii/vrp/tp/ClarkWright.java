dfaa65c4fe77c241ee19391b6b2e4c0417bf1186
Valentin <valentin.delys@gmail.com>
2017-03-14 23:07:49 +0100

    Added xreateSAvingList method in ClarkWright class

--- src/dii/vrp/tp/ClarkWright.java
+++ src/dii/vrp/tp/ClarkWright.java
@@ -3,8 +3,16 @@ package dii.vrp.tp;
 import dii.vrp.data.IDemands;
 import dii.vrp.data.IDistanceMatrix;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+/**
+ * Created by Valentin on 12/03/2017.
+ */
 public class ClarkWright
 {
+
     private IDistanceMatrix distances;
     private IDemands demands;
     private double Q;
@@ -18,8 +26,34 @@ public class ClarkWright
     public ISolution run() {
         // Calculate savings list
 
+        createSavingsList();
 
         return new VRPSolution();
+    }
+
+    public ArrayList<SavingsNode> createSavingsList() {
+
+        ArrayList<SavingsNode> savingsList = new ArrayList<>();
+
+        for (int i = 0; i < distances.size(); i++) {
+            for (int j = 0; j < distances.size(); j++) {
+                if (i != j) {
+                    double saving = distances.getDistance(0, i) + distances.getDistance(0, j) - distances.getDistance(i, j);
+
+                    savingsList.add(new SavingsNode(i, j, saving));
+                }
+            }
+        }
+
+        // Sort the savings
+        savingsList.sort(Comparator.comparing(SavingsNode::getSavings));
+        Collections.reverse(savingsList);
+
+        for (SavingsNode saving : savingsList) {
+            System.out.println(saving.getSavings());
+        }
+
+        return savingsList;
     }
 }
 
